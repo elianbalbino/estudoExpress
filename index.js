77b58a713f1dcc9c787ccda76382f7fd3405ea89
elianbalbino <elianbalbino@yahoo.com.br>
2021-03-31 14:48:35 -0300

    Iniciando CRUD

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 
-console.log(app);
+const projects = [];
+
+app.get('/projects', (request, response) => {
+  // const { title, owner } = request.query;
+
+  return response.json(projects);
+});
+
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuidv4(), title, owner };
+
+  projects.push(project); // esse push joga a criaçao do nosso projeto para o nosso array
+
+  return response.json(project); // sempre retornar o projeto recem criado e nunca exibir a lista completa
+});
+
+app.put('/projects/:id', (request, response) => {
+  const params = request.params;
+  console.log(params);
+  return response.json(['Projeto 50', 'Projeto 2', 'Projeto 3']);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json(['Projeto 2', 'Projeto 3']);
+});
+
+//console.log(app);
+
+app.listen(3000);
