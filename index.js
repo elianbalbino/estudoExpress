9fb4cfcbede822afd95102ea4c8708b3fa3477f9
elianbalbino <elianbalbino@yahoo.com.br>
2021-04-01 11:09:21 -0300

    Refatorando Crud

--- index.js
+++ index.js
@@ -23,13 +23,42 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
-  const params = request.params;
-  console.log(params);
-  return response.json(['Projeto 50', 'Projeto 2', 'Projeto 3']);
+  const { id } = request.params; // aqui pegamos nosso ID
+  const { title, owner } = request.body; //retornando uma nova informação
+
+  //aqui usamos o findIndex para percorrer todo o array atrás do id
+  //findIndex vai percorrer todos os projetos, e toda vez que ele percorrer na variável PROJECT
+  //caso ela seja satisfeita e retorne TRUE, ela vai retornar o id que estou passando (project=> project.id ===id)
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Projeto não encontrado' });
+  }
+
+  //agora que tenho indice , vou criar uma nova informação do projeto
+  const project = {
+    id,
+    title,
+    owner,
+  }
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json(['Projeto 2', 'Projeto 3']);
+  const {id} = request.params
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Projeto não encontrado' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.json(204).send();
 });
 
 //console.log(app);
